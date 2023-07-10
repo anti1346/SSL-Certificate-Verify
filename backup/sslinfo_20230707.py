e53e27ff5d4f59ad3f6b197946cd7f21655b883d
Aby <anti1346@gmail.com>
2023-07-10 11:03:52 +0900

    no message

--- backup/sslinfo_20230707.py
+++ backup/sslinfo_20230707.py
@@ -1,24 +1,13 @@
 import ssl
 import socket
 from datetime import datetime
-import argparse
 
 def get_ssl_info(domain):
     try:
-        # 도메인과 포트 분리
-        if ':' in domain:
-            domain, port = domain.split(':')
-            port = int(port)
-        else:
-            port = 443
-
-        # 도메인의 IP 주소 가져오기
-        ip = socket.gethostbyname(domain)
-
         # 도메인에 대한 소켓 객체 가져오기
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(10)
-        sock.connect((ip, port))
+        sock.connect((domain, 443))
 
         # 소켓 객체를 SSL 컨텍스트로 감싸기
         context = ssl.create_default_context()
@@ -39,11 +28,31 @@ def get_ssl_info(domain):
         if 'organizationName' in issuer:
             registrar = issuer['organizationName']
 
+        # 도메인의 IP 주소 가져오기
+        ip = socket.gethostbyname(domain)
+
         return domain, ip, start_date, expiration_date, int(remaining_days), registrar
-    except (socket.timeout, socket.gaierror, ssl.SSLCertVerificationError):
+    except (socket.timeout, ssl.SSLCertVerificationError):
         return domain, "N/A", "N/A", "N/A", "N/A", "N/A"
 
-def generate_html_table(table_rows):
+if __name__ == '__main__':
+    # 파일에서 도메인 이름 가져오기
+    with open('domains.txt', 'r') as f:
+        domains = f.readlines()
+
+    table_rows = []
+
+    # 각 도메인에 대한 SSL 정보 가져오기
+    for domain in domains:
+        ssl_info = get_ssl_info(domain.strip())
+
+        # SSL 정보를 테이블 행에 추가하기
+        table_rows.append(ssl_info)
+
+    # "N/A" 값을 처리하여 남은 일 수(`remaining_days`)를 기준으로 테이블 행 정렬하기 (오름차순)
+    table_rows.sort(key=lambda x: x[4] if isinstance(x[4], int) else float('inf'))
+
+    # HTML 테이블 생성하기
     html_table = "<html>"
     html_table += "<head>"
     html_table += "<meta charset='utf-8'>"
@@ -95,38 +104,12 @@ def generate_html_table(table_rows):
     html_table += "</body>"
     html_table += "</html>"
 
-    return html_table
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='SSL Certificate Expiration Date Checker')
-    parser.add_argument('input_file', help='Path to the input file (domains.txt)')
-    parser.add_argument('output_file', help='Path to the output file (result.html)')
-    args = parser.parse_args()
-
-    # 파일에서 도메인 이름 가져오기
-    with open(args.input_file, 'r') as f:
-        domains = f.readlines()
-
-    table_rows = []
-
-    # 각 도메인에 대한 SSL 정보 가져오기
-    for domain in domains:
-        ssl_info = get_ssl_info(domain.strip())
-
-        # SSL 정보를 테이블 행에 추가하기
-        table_rows.append(ssl_info)
-
-    # "N/A" 값을 처리하여 남은 일 수(`remaining_days`)를 기준으로 테이블 행 정렬하기 (오름차순)
-    table_rows.sort(key=lambda x: x[4] if isinstance(x[4], int) else float('inf'))
-
-    # HTML 테이블 생성하기
-    html_table = generate_html_table(table_rows)
-
-    # HTML 테이블을 결과 파일에 저장하기
-    with open(args.output_file, 'w') as file:
+    # HTML 테이블을 result.html 파일에 저장하기
+    with open('result.html', 'w') as file:
         file.write(html_table)
 
     print("SSL 정보가 result.html 파일에 저장되었습니다.")
+    
 
 ### SSL 인증서 만료일 조회
 # echo "" | openssl s_client -connect example.com:443 2>/dev/null | openssl x509 -noout -startdate -enddate
